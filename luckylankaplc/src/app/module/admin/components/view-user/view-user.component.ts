1c1c3ab03433af869db9d4dd873d8dfeb6c5ad1e
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-03-25 01:31:34 +0530

    closed #1

--- luckylankaplc/src/app/module/admin/components/view-user/view-user.component.ts
+++ luckylankaplc/src/app/module/admin/components/view-user/view-user.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-view-user',
   templateUrl: './view-user.component.html',
   styleUrl: './view-user.component.css'
 })
-export class ViewUserComponent {
+export class ViewUserComponent implements OnInit{
+
+  users: any[] = [];
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  constructor(private adminService : AdminService){
+
+  }
+
+  getUsers(){
+
+    this.adminService.getUsers().subscribe(
+      (response) => {
+        this.users = response;
+      },
+      (error) =>{
+        console.error('Error fetching users', error); 
+      }
+    );
+
+  }
 
 }
--- luckylankaplc/src/app/module/admin/services/admin.service.ts
+++ luckylankaplc/src/app/module/admin/services/admin.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { StorageService } from '../../../auth/services/storage-service/storage.service';
+import { Observable } from 'rxjs';
+
+const BASE_URL = ["http://localhost:8080"];
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+
+  constructor(private http : HttpClient) { }
+
+    createAuthorizationHeader(): HttpHeaders {
+      const token = StorageService.getToken(); 
+      if (token) {
+        return new HttpHeaders({
+          'Authorization': 'Bearer ' + token
+        });
+      } else {
+        return new HttpHeaders(); 
+      }
+    }
+    
+
+  getUsers() : Observable<any>{
+
+    return this.http.get(BASE_URL + "/luckylanka/hrandpro/admin/getusers",{
+      headers: this.createAuthorizationHeader()
+    }); 
+  }
 }
